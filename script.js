4d59567cc7004cfdc50b9f9198ed0b6944a5e16c
kleon6214 <kermitld21@gmail.com>
2026-06-15 12:12:44 -0400

    completed human and computer logic

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-console.log("Hello, World!");
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+
+    if (randomNumber === 0) {
+        return "Rock"
+
+    }
+
+    else if (randomNumber === 1) {
+        return "Paper"
+
+    }
+
+    else {
+        return "Scissors"
+
+    }
+}
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+    window.prompt("Enter Rock, Paper or Scissors")
+
+
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        return "Human Wins"
+
+
+    }
+}
+console.log(playRound());
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+function playGame() {
+
+}
